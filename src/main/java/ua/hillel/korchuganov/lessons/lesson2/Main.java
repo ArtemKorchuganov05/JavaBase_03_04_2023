ee3df5221977dcdc4686ff857051a4791c249dba
Korchuganov Artem <artemkorchuganov05@gmail.com>
2023-04-11 16:42:45 +0300

    commit all files

---
+++ src/main/java/ua/hillel/korchuganov/lessons/lesson2/Main.java
@@ -0,0 +1,69 @@
+package ua.hillel.korchuganov.lessons.lesson2;
+
+public class Main {
+    public static void main(String[] args) {
+        // byte age;
+
+        //   age = 28;
+
+        //  System.out.println(age);
+
+        // short shortVariable = 32500;
+
+        //  int intVariable = 2_000_222_444;
+
+        //  long longVariable = 4_454_452_232_546L;
+
+        float floatVariable = 55.12345678911234567890f;
+        double doubleVariable = 55.12345678911234567890;
+
+
+        char symbol1 = 'Й';
+        char symbol2 = 65;
+        char symbol3 = '\u1381';
+
+
+        System.out.println(symbol1);
+        System.out.println(symbol2);
+
+        boolean booleanVariable = true;
+        boolean booleanVariableFalse = false;
+
+        System.out.println(booleanVariable);
+        System.out.println(booleanVariableFalse);
+
+        String str = "Hello";
+
+        System.out.println("Hello" + "workd");
+//
+//        int a = 10;
+//        Integer b = 15;
+//
+//        System.out.println(Byte.MAX_VALUE);
+//
+//        byte byteVariable = 50;
+//
+//        short shortVariable = byteVariable;
+//        long longVariable = byteVariable;
+//
+//        char ch = 'A';
+//
+//        int age = 28;
+//
+//        byte byteAge = (byte) age;
+//
+//        System.out.println(byteAge);
+        System.out.println((byte) 126);
+        System.out.println((byte) 127);
+        System.out.println((byte) 128);
+        System.out.println((byte) 126);
+        System.out.println((byte) 126);
+        System.out.println((byte) 126);
+
+        double PI_VALUE = 3.14115;
+
+        PI_VALUE = 3.14;
+
+        System.out.println((int) 55.88);
+    }
+}
