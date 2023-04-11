32a8b95c9fcf55a91746c470f825f14f97b7a83b
Korchuganov Artem <artemkorchuganov05@gmail.com>
2023-04-11 17:21:26 +0300

    test commit

--- src/main/java/ua/hillel/korchuganov/lessons/lesson3/Main1.java
+++ src/main/java/ua/hillel/korchuganov/lessons/lesson3/Main1.java
@@ -3,5 +3,6 @@ package ua.hillel.korchuganov.lessons.lesson3;
 public class Main1 {
     public static void main(String[] args) {
         System.out.println("Hello, Main1!");
+        System.out.println("test");
     }
 }
