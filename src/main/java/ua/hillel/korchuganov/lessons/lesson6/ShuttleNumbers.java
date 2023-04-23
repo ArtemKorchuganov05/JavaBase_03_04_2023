c6747283e91d68da7ab244cc1d59b7f2cf860395
Artem Korchuganov <artemkorchuganov05@gmail.com>
2023-04-24 01:01:55 +0300

    changed ShuttleNumbers.java

--- src/main/java/ua/hillel/korchuganov/lessons/lesson6/ShuttleNumbers.java
+++ src/main/java/ua/hillel/korchuganov/lessons/lesson6/ShuttleNumbers.java
@@ -4,17 +4,14 @@ public class ShuttleNumbers {
     public static void main(String[] args) {
         int count = 0;
         for (int i = 1; i <= 300; i++) {
-            if (i % 10 == 4 || i % 10 == 9 || i / 10 == 4 || i / 10 == 9) {
-                continue;
-            }
-            if (i / 100 == 4 || i / 100 == 9 || (i / 10) % 10 == 4 || (i / 10) % 10 == 9 || i % 10 == 4 || i % 10 == 9) {
+            if (i % 10 == 4 || i % 10 == 9 || i / 10 == 4 || i / 10 == 9 || i / 10 % 10 == 4 || i / 10 % 10 == 9) {
                 continue;
             }
 
             System.out.println(i);
             count++;
 
-            if (count == 100) {
+            if (count == 500) {
                 System.out.println("Знайдено " + count + " номерів шатлів.");
                 break;
             }
