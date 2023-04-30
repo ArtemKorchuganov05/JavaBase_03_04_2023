50505eef5d1e55bcf5cc30b5c5c262accf0ae86e
Artem Korchuganov <artemkorchuganov05@gmail.com>
2023-04-30 14:17:36 +0300

    added Main.java

---
+++ src/main/java/ua/hillel/korchuganov/lessons/lesson8/Main.java
@@ -0,0 +1,154 @@
+package ua.hillel.korchuganov.lessons.lesson8;
+
+import java.util.Arrays;
+//import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // O(N)
+//        for (int i = 0; i < 5; i++) {
+//
+//        }
+//
+//        int counter = 0;
+//        // O(N^2)
+//        for (int i = 0; i < 15; i++) {
+//            for (int j = 0; j < 12; j++) {
+//                counter++;
+//            }
+//        }
+//
+//         counter = 0;
+//
+//        // O(N^3)
+//        for (int i = 0; i < 15; i++) {
+//            for (int j = 0; j < 12; j++) {
+//                for (int k = 0; k < 3; k++) {
+//                    counter++;
+//                }
+//
+//            }
+//        }
+//        System.out.println("counter: " + counter);
+
+//        int[] array = {1, 5, 25, 67, 443, 77, 43, 344, 22};
+
+//        int counter = 0;
+
+//        for (int i = 0; i < array.length - 1; i++) {
+//            for (int j = 0; j < array.length - 1 - i; j++) {
+//                counter++;
+//                if (j % 2 != 0) {
+//
+//                }
+//                if (array[j] > array[j + 1]) {
+//                    int temp = array[j];
+//                    array[j] = array[j + 1];
+//                    array[j + 1] = temp;
+//                }
+//            }
+//        }
+//        System.out.println(Arrays.toString(array));
+//       System.out.println(counter);
+
+//        int counter = 0;
+//        boolean swapped = true;
+//        int start = 0;
+//        int end = array.length;
+//
+//        while (swapped == true) {
+//            swapped = false;
+//
+//            for (int i = start; i < end - 1; ++i) {
+//                if (array[i] > array[i + 1]) {
+//                    counter++;
+//                    int temp = array[i];
+//                    array[i] = array[i + 1];
+//                    array[i + 1] = temp;
+//                    swapped = true;
+//                }
+//            }
+//
+//            if (swapped == false) {
+//                break;
+//            }
+//            swapped = false;
+//            end = end - 1;
+//
+//            for (int i = end - 1; i >= start; i--) {
+//                counter++;
+//                if (array[i] > array[i + 1]) {
+//                    int temp = array[i];
+//                    array[i] = array[i + 1];
+//                    array[i + 1] = temp;
+//                    swapped = true;
+//                }
+//            }
+//            start = start + 1;
+//        }
+//        System.out.println(Arrays.toString(array));
+//        System.out.println(counter);
+
+//        Integer[] array = {1, 5, 25, 67, 443, 77, 43, 344, 22};
+//
+//        Arrays.sort(array);
+//        System.out.println(Arrays.toString(array));
+//
+//        Arrays.sort(array, Collections.reverseOrder());
+//        System.out.println(Arrays.toString(array));
+
+//        int[] array = {1,2,3,4,5,6,7,8,9,10};
+//        int element = 8;
+//        int index = -1;
+//        int counter = 0;
+
+
+//      O(N) - BAD VERSION
+//        for (int i = 0; i < array.length; i++) {
+//            counter++;
+//            if (array[i] ==element) {
+//                index = i;
+//                break;
+//            }
+//        }
+
+        //O(log2N) -> O(logN)
+//        int firstIndex = 0;
+//        int lastIndex = array.length - 1;
+//
+//        while(firstIndex <= lastIndex){
+//            counter++;
+//            int middleIndex = (firstIndex + lastIndex) / 2;
+//
+//            if(array[middleIndex] == element){
+//                index = middleIndex;
+//                break;
+//            }else if(array[middleIndex] < element){
+//                firstIndex = middleIndex + 1;
+//            }else{
+//                lastIndex = middleIndex - 1;
+//            }
+//
+//        }
+//        System.out.println("index = " + index);
+//        System.out.println(counter);
+//
+//        int indexArray = Arrays.binarySearch(array, element);
+//        System.out.println("binarySearch: " + indexArray);
+
+//        int[] array = {1,2,3,4};
+//        int[] array2 = Arrays.copyOf(array, 10);
+//        System.out.println(Arrays.toString(array2));
+
+        int[] array = {1,2,3};
+        int oldLength = array.length;
+       array = Arrays.copyOf(array,array.length + 3);
+        int number = 4;
+        for (int i = oldLength; i < array.length; i++) {
+            array[i] = number++;
+        }
+
+        System.out.println(Arrays.toString(array));
+    }
+}
