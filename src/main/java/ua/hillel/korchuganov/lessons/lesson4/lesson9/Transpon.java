c7238def391fd0adccf2c1505aa9e54a9d586146
Artem Korchuganov <artemkorchuganov05@gmail.com>
2023-05-01 21:56:46 +0300

    added Transpon.java

---
+++ src/main/java/ua/hillel/korchuganov/lessons/lesson4/lesson9/Transpon.java
@@ -0,0 +1,38 @@
+package ua.hillel.korchuganov.lessons.lesson4.lesson9;
+
+public class Transpon {
+    public static void main(String[] args) {
+        int[][] array = new int[4][4];
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = (int) (Math.random() * 101);
+            }
+        }
+
+        for (int i = 0; i < array.length; i++) {
+                for (int j = 0; j < array[i].length; j++) {
+                    System.out.print(array[i][j] + "\t");
+                }
+                System.out.println();
+            }
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array[i].length; j++) {
+                int temp = array[i][j];
+                array[i][j] = array[j][i];
+                array[j][i] = temp;
+            }
+        }
+        System.out.println();
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+        }
+    }
+
