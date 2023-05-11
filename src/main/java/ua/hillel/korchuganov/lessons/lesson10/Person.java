b43d6a7c69be1d48444a71b6d8a0c559392b4e4d
Artem Korchuganov <artemkorchuganov05@gmail.com>
2023-05-11 12:29:49 +0300

    added Person.java

---
+++ src/main/java/ua/hillel/korchuganov/lessons/lesson10/Person.java
@@ -0,0 +1,13 @@
+package ua.hillel.korchuganov.lessons.lesson10;
+
+public class Person {
+    public static void main(String[] args) {
+        System.out.println(personInfo("Will", "Smith", "New York", "2936729462846"));
+        System.out.println(personInfo("Jackie", "Chan", "Shanghai", "12312412412"));
+        System.out.println(personInfo("Sherlock", "Holmes", "London", "37742123513"));
+    }
+
+    static String personInfo(String name,String surname, String city, String phone){
+        return "Зателефонувати громадянинові " + name + " " + surname + " із міста " + city +  " можна за номером " + phone + ".";
+    }
+}
