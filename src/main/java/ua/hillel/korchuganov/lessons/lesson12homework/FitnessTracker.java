d8389bde0590335fc73a9b07d8983cc0064ea699
Artem Korchuganov <artemkorchuganov05@gmail.com>
2023-05-31 00:12:57 +0300

    changed lesson12homework

--- src/main/java/ua/hillel/korchuganov/lessons/lesson12homework/FitnessTracker.java
+++ src/main/java/ua/hillel/korchuganov/lessons/lesson12homework/FitnessTracker.java
@@ -3,61 +3,96 @@ package ua.hillel.korchuganov.lessons.lesson12homework;
 public class FitnessTracker {
     private String firstName;
     private String lastName;
-    private int birthday;
-    private int birthMonth;
-    private int birthYear;
+    private int dayOfBirth;
+    private int monthOfBirth;
+    private int yearOfBirth;
     private String email;
     private String phone;
-    private double weight;
+    private int weight;
     private String bloodPressure;
-    private int stepsDay;
+    private int dailySteps;
     private int age;
 
-
-    public FitnessTracker(String firstName, int birthday, int birthMonth, int birthYear, String email, String phone) {
+    public FitnessTracker(String firstName, String lastName, int dayOfBirth, int monthOfBirth, int yearOfBirth, String email, String phone) {
         this.firstName = firstName;
-        this.birthday = birthday;
-        this.birthMonth = birthMonth;
-        this.birthYear = birthYear;
+        this.lastName = lastName;
+        this.dayOfBirth = dayOfBirth;
+        this.monthOfBirth = monthOfBirth;
+        this.yearOfBirth = yearOfBirth;
         this.email = email;
         this.phone = phone;
-        calculateAge();
+        this.age = calculateAge(yearOfBirth);
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    private int calculateAge(int yearOfBirth) {
+        return 2020 - yearOfBirth;
     }
 
-    public void setWeight(double weight) {
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getDayOfBirth() {
+        return dayOfBirth;
+    }
+
+    public int getMonthOfBirth() {
+        return monthOfBirth;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
         this.weight = weight;
+    }
+
+    public String getBloodPressure() {
+        return bloodPressure;
     }
 
     public void setBloodPressure(String bloodPressure) {
         this.bloodPressure = bloodPressure;
     }
 
-    public void setStepsDay(int stepsDay) {
-        this.stepsDay = stepsDay;
+    public int getDailySteps() {
+        return dailySteps;
     }
 
-    public void calculateAge() {
-        int currentYear = 2023;
-        age = currentYear - birthYear;
+    public void setDailySteps(int dailySteps) {
+        this.dailySteps = dailySteps;
+    }
 
-        if (birthMonth > 5 || (birthMonth == 5 && birthday > 25)) {
-            age--;
-        }
+    public int getAge() {
+        return age;
     }
 
     public void printAccountInfo() {
         System.out.println("Name: " + firstName + " " + lastName);
-        System.out.println("Date of Birth: " + birthday + "-" + birthMonth + "-" + birthYear);
+        System.out.println("Date of Birth: " + dayOfBirth + "-" + monthOfBirth + "-" + yearOfBirth);
         System.out.println("Email: " + email);
         System.out.println("Phone: " + phone);
         System.out.println("Surname: " + lastName);
         System.out.println("Weight " + weight + " kg");
         System.out.println("Blood pressure: " + bloodPressure);
-        System.out.println("Steps per Day: " + stepsDay);
+        System.out.println("Steps per Day: " + dailySteps);
         System.out.println("Age: " + age);
         System.out.println();
     }
--- src/main/java/ua/hillel/korchuganov/lessons/lesson12homework/MainTracker.java
+++ src/main/java/ua/hillel/korchuganov/lessons/lesson12homework/MainTracker.java
@@ -2,26 +2,22 @@ package ua.hillel.korchuganov.lessons.lesson12homework;
 
 public class MainTracker {
     public static void main(String[] args) {
-        FitnessTracker user1 = new FitnessTracker("John", 10,5,1990, "john@example.com", "1234567890");
-        user1.setLastName("Doe");
-        user1.setWeight(73.5);
+        FitnessTracker user1 = new FitnessTracker("John", "Doe", 15, 6, 1990, "john.doe@example.com", "1234567890");
+        user1.setWeight(75);
         user1.setBloodPressure("120/80");
-        user1.setStepsDay(10000);
+        user1.setDailySteps(8000);
 
-        FitnessTracker user2 = new FitnessTracker("Jane", 15,8,1995, "jane@example.com", "0987654321");
-        user2.setLastName("Smith");
-        user2.setWeight(65.5);
+        FitnessTracker user2 = new FitnessTracker("Jane", "Smith", 25, 9, 1985, "jane.smith@example.com", "9876543210");
+        user2.setWeight(62);
         user2.setBloodPressure("110/70");
-        user2.setStepsDay(8000);
+        user2.setDailySteps(10000);
 
-        FitnessTracker user3 = new FitnessTracker("Mike", 20,12,1992, "mike@example.com", "1234564390");
+        FitnessTracker user3 = new FitnessTracker("Alice", "Johnson", 10, 3, 1995, "alice.johnson@example.com", "5555555555");
+        user3.setWeight(68);
+        user3.setBloodPressure("130/85");
+        user3.setDailySteps(6000);
 
-        user3.setLastName("Le");
-        user3.setWeight(68.5);
-        user3.setBloodPressure("100/70");
-        user3.setStepsDay(9000);
-
-
+        // Вивід інформації про користувачів
         user1.printAccountInfo();
         user2.printAccountInfo();
         user3.printAccountInfo();
