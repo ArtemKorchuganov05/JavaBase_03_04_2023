9cea7a9aaa15f07bf8fb704178198b0e02b2265f
Artem Korchuganov <artemkorchuganov05@gmail.com>
2023-06-04 20:57:49 +0300

    added lesson16homework

--- src/test/java/ua/hillel/korchuganov/lesson16homework/SquareMatrixCheckerTest.java
+++ src/test/java/ua/hillel/korchuganov/lesson16homework/SquareMatrixCheckerTest.java
@@ -2,17 +2,21 @@ package ua.hillel.korchuganov.lesson16homework;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import ua.hillel.korchuganov.lessons.lesson16homework.SquareMatrixCheker;
+
+import static ua.hillel.korchuganov.lessons.lesson16homework.SquareMatrixCheker.isSquareMatrix;
 
 public class SquareMatrixCheckerTest {
+
     @Test
-    public boolean testIsSquareMatrix() {
+    public void testIsSquareMatrix() {
         int[][] squareMatrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         int[][] nonSquareMatrix = {{1, 2, 3}, {4, 5, 6}};
         int[][] emptyMatrix = {};
         int[][] nullMatrix = null;
 
-        Assertions.assertTrue(testIsSquareMatrix());
-        Assertions.assertFalse(testIsSquareMatrix());
-        return false;
-    }
-}
+        Assertions.assertTrue(isSquareMatrix(squareMatrix));
+        Assertions.assertFalse(isSquareMatrix(nonSquareMatrix));
+        Assertions.assertFalse(isSquareMatrix(emptyMatrix));
+
+    }}
