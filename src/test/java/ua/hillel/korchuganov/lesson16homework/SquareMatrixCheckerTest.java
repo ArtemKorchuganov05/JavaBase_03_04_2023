fb6b25fd26686baf334670187b4938c01c5ade54
Artem Korchuganov <artemkorchuganov05@gmail.com>
2023-06-05 12:56:00 +0300

    changed lesson16homework

--- src/test/java/ua/hillel/korchuganov/lesson16homework/SquareMatrixCheckerTest.java
+++ src/test/java/ua/hillel/korchuganov/lesson16homework/SquareMatrixCheckerTest.java
@@ -19,4 +19,5 @@ public class SquareMatrixCheckerTest {
         Assertions.assertFalse(isSquareMatrix(nonSquareMatrix));
         Assertions.assertFalse(isSquareMatrix(emptyMatrix));
 
-    }}
+    }
+}
